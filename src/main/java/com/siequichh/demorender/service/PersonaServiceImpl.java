b0aa1f1423639e61cd44a4cdab0421246a24a753
Deyvid <143696900+Siequichh@users.noreply.github.com>
2024-12-20 03:28:07 -0500

    target jar
    jar para deploy

--- src/main/java/com/siequichh/demorender/service/PersonaServiceImpl.java
+++ src/main/java/com/siequichh/demorender/service/PersonaServiceImpl.java
@@ -4,6 +4,7 @@ import com.siequichh.demorender.dao.PersonaDAO;
 import com.siequichh.demorender.entity.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ public class PersonaServiceImpl implements PersonaService {
         return personaDAO.findAll();
     }
     @Override
+    @Transactional
     public List<Persona> searchPersonaByNombre(String nombre) {
         return personaDAO.findByNombreContaining(nombre);
     }
